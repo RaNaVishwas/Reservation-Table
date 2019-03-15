3b9f32cfd7071a85531dc90d6c04332a4a64e63e
Vishwas RN <vreddynaga@hawk.iit.edu>
2019-03-15 18:54:36 -0500

    Update AdminControllerChip.java

--- AdminControllerChip.java
+++ AdminControllerChip.java
@@ -200,3 +200,107 @@ private void goHomeScreen(ActionEvent event) {
     } catch (IOException e) {
         e.printStackTrace();
     }
+Scene scene = new Scene(root, 1024, 720);
+    stage.setTitle("Restaurant Reservation System");
+    stage.setScene(scene);
+    stage.show();
+}
+
+@FXML
+private void reservationsButtonClicked(ActionEvent event) {
+    configureButtons();
+    reservationsButton.setGraphic(reservationsSelectedIMV);
+
+    //Clear the content of contentPane
+    contentPane.getChildren().clear();
+
+    //Set title
+    titleLabel.setText("Reservations");
+
+    // Make box for weekly availability and reservations
+    VBox box = new VBox();
+    box.setSpacing(10);
+    box.setAlignment(Pos.TOP_CENTER);
+    box.setPrefWidth(900);
+
+    // box for availability
+    VBox abox = new VBox();
+    box.setSpacing(10);
+    box.setAlignment(Pos.TOP_CENTER);
+    box.setPrefWidth(900);
+
+    // box for reservations
+    VBox resbox = new VBox();
+    box.setSpacing(10);
+    box.setAlignment(Pos.TOP_CENTER);
+    box.setPrefWidth(900);
+
+    //set up for weekly table availability
+    Text aHeader = new Text("Tables still available");
+    aHeader.setFont(new Font("System", 24));
+
+    TableView aTable = new TableView();
+    TableColumn aDateCol = new TableColumn("Date");
+    aDateCol.setCellValueFactory(new PropertyValueFactory<>("date"));
+    TableColumn tablesCol = new TableColumn("Tables Available");
+    tablesCol.setCellValueFactory(new PropertyValueFactory<>("tablesAvailable"));
+
+    //add table and title to availability box
+    abox.getChildren().addAll(aHeader, aTable);
+    abox.setSpacing(5);
+
+    //set width for all cols
+    aDateCol.setMinWidth(100);
+    tablesCol.setMinWidth(100);
+
+    aTable.getColumns().addAll(aDateCol, tablesCol);
+    //Populate data to the table view
+    operation.callDates();
+    ObservableList<Availability> adata = FXCollections.observableArrayList(operation.getWeeklyAvailability());
+    aTable.setItems(adata);
+    aTable.setMaxHeight(235);
+    aTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+    //set up for reservations
+    Text resHeader = new Text("All Reservations");
+    resHeader.setFont(new Font("System", 24));
+
+    TableView resTable = new TableView();
+    TableColumn firstNameCol = new TableColumn("First Name");
+    firstNameCol.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+    TableColumn lastNameCol = new TableColumn("Last Name");
+    lastNameCol.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+    TableColumn tidCol = new TableColumn("tID");
+    tidCol.setCellValueFactory(new PropertyValueFactory<>("tID"));
+    TableColumn partySizeCol = new TableColumn("Party Size");
+    partySizeCol.setCellValueFactory(new PropertyValueFactory<>("partySize"));
+    TableColumn seatsCol = new TableColumn("Seats");
+    seatsCol.setCellValueFactory(new PropertyValueFactory<>("seats"));
+    TableColumn dateCol = new TableColumn("Reservation Date");
+    dateCol.setCellValueFactory(new PropertyValueFactory<>("reservationDate"));
+
+    //set width for all cols
+    firstNameCol.setMinWidth(100);
+    lastNameCol.setMinWidth(100);
+    tidCol.setMinWidth(100);
+    partySizeCol.setMinWidth(100);
+    seatsCol.setMinWidth(100);
+    dateCol.setMinWidth(300);
+
+    //add table and title to availability box
+    ScrollPane scrollPane = new ScrollPane();
+    scrollPane.setContent(resTable);
+    scrollPane.setFitToWidth(true);
+    resbox.getChildren().addAll(resHeader, scrollPane);
+    resbox.setSpacing(5);
+
+    resTable.getColumns().addAll(firstNameCol, lastNameCol, tidCol, partySizeCol, seatsCol, dateCol);
+    //Populate data to the table view
+    ObservableList<ReservationInfo> resdata = FXCollections.observableArrayList(operation.getAllReservations());
+    resTable.setItems(resdata);
+    resTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+    //add table view to content pane
+    box.getChildren().addAll(abox, resbox);
+    contentPane.getChildren().add(box);
+}
